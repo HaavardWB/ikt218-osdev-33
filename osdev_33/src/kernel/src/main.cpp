9974f62b05710326f33fb90e597c0b222cff11c3
Håvard Willoch Bergsvik <haavardwb@uia.no>
2023-05-31 13:49:28 +0200

    committen

--- osdev_33/src/kernel/src/main.cpp
+++ osdev_33/src/kernel/src/main.cpp
@@ -1,12 +1,13 @@
 
-#include "system.h"
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
+    #include "../../stdlib/include/system.h"
     void kernel_main();
 }
-
 void kernel_main()
 {
-
+    int a = 10;
+    int b = 20;
+    char* tekst = "Heisann!";
 }
--- osdev_33/src/stdlib/src/system.c
+++ osdev_33/src/stdlib/src/system.c
@@ -1,1 +1,10 @@
 #include "system.h"
+
+//koden til strlen AKA implimenterer libC funksjonen
+size_t strlen(const char* str) 
+{
+	size_t len = 0;
+	while (str[len])
+		len++;
+	return len;
+}
